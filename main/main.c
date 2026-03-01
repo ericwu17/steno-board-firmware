244757906167fc5c557cab0bd91354c02fd40905
Eric Wu <eric.dianhao.wu@gmail.com>
2026-03-01 13:23:18 -0800

    update main: make LED blink.

--- main/main.c
+++ main/main.c
@@ -17,24 +17,12 @@ void app_main(void) {
     xTaskCreate(stroke_handler_task, "stroke_handler_task", 3 * 1024, NULL, configMAX_PRIORITIES - 3,
                 &stroke_handler_task_hdl);
 
+    int led1state = 0;
     while(1) {
         vTaskDelay(20 / portTICK_PERIOD_MS);
-        // if (is_connected()) {
-        //     ESP_LOGI("XXX", "CONNECTED");
 
-        //     vTaskDelay(3000 / portTICK_PERIOD_MS);
-        //     send_string("Hello Rila.");
-        //     vTaskDelay(1000 / portTICK_PERIOD_MS);
-        //     send_n_deletes(5);
-        //     send_string("Eric.");
-        //     vTaskDelay(1000 / portTICK_PERIOD_MS);
-        //     send_n_deletes(5 + 6);
-        // }
-
-
-        // int n = read_switches();
-        // ESP_LOGI("XXX", "SWITCHES %i", n);
-        // set_led_1(n & 1);
-        // set_led_2((n >> 1) & 1);
+        vTaskDelay(1000/portTICK_PERIOD_MS);
+        set_led_1(led1state);
+        led1state = !led1state;
     }
 }
