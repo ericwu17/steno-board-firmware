0c38f0ee91d504ffd488700e67541c281fedb941
Eric Wu <eric.dianhao.wu@gmail.com>
2026-03-01 14:34:26 -0800

    update partition table and add new sections
    included some temporary function to prevent optimizing away the
    new sections.
    
    New sections are in .h files that have not yet been committed to git.

--- main/main.c
+++ main/main.c
@@ -4,9 +4,34 @@
 
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "key_section.h"
+#include "str_section.h"
+#include "val_section.h"
 
 TaskHandle_t stroke_reader_task_hdl;
 TaskHandle_t stroke_handler_task_hdl;
+
+// temporary function to avoid optimizations
+void count_zeros() {
+    int count = 0;
+    for (int i = 0; i < key_section_size; i ++) {
+        if (key_section[i] == 0) {
+            count += 1;
+        }
+    }
+    for (int i = 0; i < str_section_size; i ++) {
+        if (str_section[i] == 0) {
+            count += 1;
+        }
+    }
+    for (int i = 0; i < val_section_size; i ++) {
+        if (val_section[i] == 0) {
+            count += 1;
+        }
+    }
+
+    ESP_LOGI("XXX", "ZERO COUNT IS %x", count);
+}
 
 void app_main(void) {
     init_keyboard();
@@ -24,5 +49,11 @@ void app_main(void) {
         vTaskDelay(1000/portTICK_PERIOD_MS);
         set_led_1(led1state);
         led1state = !led1state;
+
+        count_zeros();
+
     }
 }
+
+
+
